316eed0923605f0283713abd4b6b142cb86e42e5
Yuri Morales Britto <ybritto90@gmail.com>
2025-11-14 14:19:24 +0100

    Add initial project structure with Docker Compose, Maven POM files for API and backend, and OpenAPI specifications for user authentication endpoints. Include .gitignore for API target directory and schemas for authentication responses.

--- backend/src/main/java/com/ybritto/teamtempo/backend/authentication/entity/UserEntity.java
+++ backend/src/main/java/com/ybritto/teamtempo/backend/authentication/entity/UserEntity.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -82,6 +84,27 @@ public class UserEntity implements UserDetails {
 
         @Column(name = "updated_at", nullable = false)
         private LocalDateTime updatedAt;
+
+        @PrePersist
+        private void prePersist() {
+                if (this.uuid == null) {
+                        this.uuid = UUID.randomUUID();
+                }
+                if (this.createdAt == null) {
+                        this.createdAt = LocalDateTime.now();
+                }
+                if (this.updatedAt == null) {
+                        this.updatedAt = LocalDateTime.now();
+                }
+                if (this.role == null) {
+                        this.role = SecurityRoleEnum.USER;
+                }
+        }
+
+        @PreUpdate
+        private void preUpdate() {
+                this.updatedAt = LocalDateTime.now();
+        }
 
         @Override
         public Collection<? extends GrantedAuthority> getAuthorities() {
